e342df5689bb1bfbf9ae6f08fb60de4057ac1f57
AKSJFDS <2502371043@qq.com>
2024-03-05 00:44:26 +0800

    无

---
+++ 视频洗码.py
@@ -0,0 +1,9 @@
+
+
+
+path = r"C:\Users\AKSJF\Downloads\[極彩花夢][ばにぃうぉ～か～]OVA サキュバス喚んだら義母が来た！？ ＃2\1080P60FPS\[KyokuSai][ばにぃうぉ～か～]OVA サキュバス喚んだら義母が来た！？ ＃2[1080P60FPS][x265_10bit] - 副本.mkv"
+
+with open(path, mode='ab') as f:
+    f.seek(0,2)
+    print(f.write(b'xx'))
+
